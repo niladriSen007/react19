c630c091315b62d7e5337581e03670f19bd079a5
Niladri <niladrisengit12@gmail.com>
2024-10-05 10:29:15 +0530

    useFormStatus-hook-huxn

---
+++ src/components/FormButton.tsx
@@ -0,0 +1,11 @@
+import { useFormStatus } from "react-dom";
+
+const FormButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button type="submit" disabled={pending}>
+    {pending ? "Submitting..." : "Submit"}
+  </button>
+  )
+}
+export default FormButton
